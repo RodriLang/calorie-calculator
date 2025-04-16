e0806ad1389baa427ed6d29ea189206ddd4436a5
Ezequiel Santalla <ezequielasantalla@gmail.com>
2025-04-16 00:03:15 -0300

    cambio en la tabla meal id identity

--- src/main/java/com/trainerapp/calorie_calculator/model/entity/Meal.java
+++ src/main/java/com/trainerapp/calorie_calculator/model/entity/Meal.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Table(name = "meals")
 public class Meal {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
